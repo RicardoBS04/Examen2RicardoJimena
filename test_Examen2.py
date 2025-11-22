11ea28c07591464e082929a62ed18ce8719042ca
RicardoBS04 <j.bolanos.2@estudiantec.cr>
2025-11-21 21:43:33 -0600

    Prueba UNITARIA Ricardo

---
+++ test_Examen2.py
@@ -0,0 +1,21 @@
+import unittest
+from Examen2 import MiClase
+
+class TestMiClase(unittest.TestCase):
+
+    def setUp(self):
+        self.obj = MiClase(5, 120, 12, ["Canción 1", "Canción 2", "Canción 3"], [0.8, 0.9, 0.7])
+
+    # PRUEBA 1 assertGreater 
+    def test_valencia_mayor_que_cero(self):
+        resultado = self.obj.ObtieneValencia(13579) 
+        self.assertGreater(resultado, 0)
+
+    # PRUEBA 2 assertIsNone
+    def test_mas_bailable_lista_vacia(self):
+        resultado = self.obj.ObtieneMasBailable([])
+        self.assertIsNone(resultado) 
+ 
+
+if __name__ == "__main__":
+    unittest.main()
